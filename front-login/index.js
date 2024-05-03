6a07e494411fc35cfb8f4a1b7a7adc9ef96469dc
Breno <breno.silva@luizalabs.com>
2024-05-03 12:20:31 -0300

    second commit

--- front-login/index.js
+++ front-login/index.js
@@ -53,7 +53,7 @@ const app = Vue.createApp({
                 if (!response.ok) {
                     this.exibirErro = true;
                 } else {
-                    window.location.href = 'front-game/index.html';
+                    window.location.href = '../front-game/index.html';
                 }
                 console.log('Usuario validado com sucesso.');
             } catch (error) {
